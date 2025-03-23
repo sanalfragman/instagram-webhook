9900880da3b8be8f0190d31a9dc57db5c3ff2e79
sanalfragman <sanalfragmanlar@gmail.com>
2025-03-23 21:49:33 +0300

    Host ayarını 0.0.0.0 olarak güncelledim

--- app.py
+++ app.py
@@ -16,18 +16,16 @@ def webhook():
         print("Gelen veri:", data)
         access_token = "IGAARDZCufpcfZABZAE04SUg2Tkd2R0FHZAGd2LThxa3FxQnBlcFZA2ZATNoZATJqNE1TY1RoZAVdQNXJwZAzdfU1MtSFRqMWJWYl8waHluSGE1RVNPeE9POXJncEZAsQ2ttM0ZAvUjl4U1JBU00yVl84ZAnlrb2VaWUxPR2gyczE2OGI4NU9McwZDZD"
         for entry in data.get("entry", []):
-            # Mesajlar için
             for messaging in entry.get("messaging", []):
                 if "message" in messaging and "is_echo" not in messaging["message"]:
                     sender_id = messaging["sender"]["id"]
                     message_text = messaging["message"]["text"]
-                    # Kullanıcı adını almak için API isteği
                     user_url = f"https://graph.instagram.com/v21.0/{sender_id}?fields=username&access_token={access_token}"
                     user_response = requests.get(user_url)
                     if user_response.status_code == 200:
                         username = user_response.json().get("username", "kullanıcı")
                     else:
-                        username = "kullanıcı"  # Hata durumunda varsayılan
+                        username = "kullanıcı"
                     reply_url = f"https://graph.instagram.com/v21.0/me/messages?access_token={access_token}"
                     payload = {
                         "recipient": {"id": sender_id},
@@ -38,4 +36,4 @@ def webhook():
         return "OK", 200
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
